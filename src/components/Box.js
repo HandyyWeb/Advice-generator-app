6c4de69e3f1b926d214500c367e38f43dc42827c
HandyyWeb <lebretonandy@gmail.com>
2023-04-07 20:32:42 +0400

    Nettoyage du début des fichiers inutiles

---
+++ src/components/Box.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Box = () => {
+    return (
+        <div className='container-1'>
+            <span> Advice #117 </span>
+            <p id='text-advice'>
+                "It is easy to sit up and take notice, what's difficult is getting up and tking action"
+            </p>
+        </div>
+    );
+};
+
+export default Box;
