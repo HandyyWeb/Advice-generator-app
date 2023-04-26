f43445a1d80fb7078fc5d14ec77c3f245bb5c52d
HandyyWeb <lebretonandy@gmail.com>
2023-04-26 20:15:46 +0400

    Lets go ! First commit

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Box = () => {
     return (
-        <div className='container-1'>
-            <span> Advice #117 </span>
-            <p id='text-advice'>
-                "It is easy to sit up and take notice, what's difficult is getting up and tking action"
+        <div className='container'>
+            <span id ="number" className='container--space container__advice_number container__advice_number--green container__advice_number--length'> ADVICE #117 </span>
+            <p id ="advice" className='container--space container__advice container__advice--position container__advice--cyan'>
+                "It is easy to sit up and take notice, what's difficult is getting up and taking action"
             </p>
+            <picture>
+                <source className='container--space container__divider' srcSet="./pattern-divider-desktop.svg" alt="nothing" media="(min-width:599px)"/>
+                <img className="container--space container__divider" src='./pattern-divider-mobile.svg' alt = "A straight line" />
+            </picture>
         </div>
     );
 };
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+
 const Button = () => {
 
     return (
         <div>
-            <button> Click me </button>
+            <button className="container__button" onClick={() => handleAdvice()}></button>
         </div>
     );
 };
+
+async function handleAdvice(){
+    const response = await fetch("https://api.adviceslip.com/advice")
+    const JsonData = await response.json()
+    document.getElementById("advice").innerHTML = JsonData.slip.advice
+    document.getElementById("number").innerHTML = `Advice # ${JsonData.slip.id}`
+}
 
 export default Button;
